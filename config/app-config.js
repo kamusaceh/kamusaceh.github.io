cca3ffb17fa71936b6de2e35d6b52f98a6411f3d
kamusaceh <kamusacehproject@gmail.com>
2026-09-04 15:40:27 +0700

    Update app-config.js

--- config/app-config.js
+++ config/app-config.js
@@ -2,5 +2,5 @@ window.KAMUS_CONFIG={
   "owner": "kamusaceh",
   "repo": "kamusaceh.github.io",
   "branch": "main",
-  "token": "_pat_11CNLI6WY0zWJib4xJTE0i_8v88Qd21ja3sxvUFK6itEuCjsOEEsuOPxh48b0NRS66C3YSICECrXHzhsWc"
+  "token": ""
 };
